8249b2eeb7c28dd3e8b98dd29ca78895827a5c26
Shohei Nakagawa <hukusuke1007@gmail.com>
2019-02-13 11:30:11 +0900

    Update VuetifyChildPage3.vue

--- frontend/vue-library-training/src/views/vuetify/VuetifyChildPage3.vue
+++ frontend/vue-library-training/src/views/vuetify/VuetifyChildPage3.vue
@@ -4,16 +4,16 @@
        <v-card height="600px" flat>
         <template>
           <div class="headline text-xs-center pa-5">
-            {{ title + " タブ" + selectedTab }}
+            {{ title + `${selectedTab+1}` }}
             <v-flex style="margin: 16px;" v-if="selectedTab===0">
-              <!-- タブ1のフォームを表示 -->
+              <!--  bottomNavigationTab1のフォームを表示 -->
               <v-text-field
-                v-model="tabdata0.name"
+                v-model="bottomNaviData0.name"
                 label="名前"
                 outline
                 placeholder="名前"/>
               <v-text-field
-                v-model="tabdata0.age"
+                v-model="bottomNaviData0.age"
                 label="年齢"
                 outline
                 type="number"
@@ -26,16 +26,36 @@
               </v-btn>
             </v-flex>
             <v-flex style="margin: 16px;" v-else-if="selectedTab===1">
-              <!-- タブ2のフォームを表示 -->
+              <!-- bottomNavigationTab2のフォームを表示 -->
+              <!-- https://vuetifyjs.com/en/components/tabs -->
+              <!--MenuTabの上部 -->
+              <v-tabs
+                slot="extension"
+                v-model="bottomNaviData1.model"
+                centered
+                color="cyan"
+                slider-color="yellow">
+                <v-tab v-for="index in 3" :key="index" :href="`#tab-${index}`" class="white--text">
+                  メニュー {{ index }}
+                </v-tab>
+              </v-tabs>
+              <!--MenuTabの内容 -->
+              <v-tabs-items v-model="bottomNaviData1.model">
+                <v-tab-item v-for="index in 3" :key="index" :value="`tab-${index}`">
+                  <!--indexに応じて内容を変える -->
+                  <v-card flat>
+                    <v-card-text v-text="bottomNaviData1.text + index"/>
+                  </v-card>
+                </v-tab-item>
+              </v-tabs-items>
             </v-flex>
             <v-flex style="margin: 16px;" v-else>
-              <!-- タブ3のフォームを表示 -->
+              <!-- bottomNavigationTab3のフォームを表示 -->
             </v-flex>
           </div>
         </template>
         <!-- https://vuetifyjs.com/en/components/bottom-navigation -->
         <v-bottom-nav
-          :active.sync="bottomNav"
           :value="true"
           absolute
           color="transparent">
@@ -69,7 +89,7 @@
           v-model="isSnackbar"
           color="blue"
           multi-line
-          timeout="5000">
+          :timeout="timeout">
           <div v-html="snackbarText"/>
           <v-btn dark flat
             @click="isSnackbar = false">
@@ -89,22 +109,25 @@ export default class VuetifyChildPage3 extends Vue {
   // https://vuetifyjs.com/en/components/bottom-navigation
   // https://vuetifyjs.com/en/components/snackbars
   // https://vuetifyjs.com/en/components/tabs
-  // https://vuetifyjs.com/en/components/ratings
-  // https://vuetifyjs.com/en/components/badges
-  title: string = 'v-bottom-navのサンプル'
+  title: string = 'v-bottom-navi-tab'
   /**
    * bottom-navigation
    */
   selectedTab: number = 0
-  tabdata0: any = {
+  bottomNaviData0: any = {
     name: 'ゲスト',
     age: 20,
+  }
+  bottomNaviData1: any = {
+    model: 'tab-2',
+    text: 'menu-tab',
   }
   /**
    * snakbar
    */
   isSnackbar: boolean = false
   snackbarText: string = ''
+  timeout: number = 5000
 
   /**
    * methods
@@ -114,7 +137,7 @@ export default class VuetifyChildPage3 extends Vue {
   }
   onClickTab0Btn() {
     console.log('tabBtan')
-    this.snackbarText = `名前:${this.tabdata0.name}<br>年齢:${this.tabdata0.age}`
+    this.snackbarText = `名前:${this.bottomNaviData0.name}<br>年齢:${this.bottomNaviData0.age}`
     this.isSnackbar = true
   }
 }
