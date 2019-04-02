82b8488896799efb9c78ea2b5f1a5d41a7823090
Shohei Nakagawa <hukusuke1007@gmail.com>
2019-04-03 07:53:55 +0900

    js

--- frontend/htmlcssjs/lesson/javascript/lesson_media/1/main.js
+++ frontend/htmlcssjs/lesson/javascript/lesson_media/1/main.js
@@ -1,26 +1,5 @@
-document.querySelector('#checkbox').addEventListener('change', (event) => {
-  const element = document.querySelector('.rect');
-  if (element.classList.contains('state-show') === true) {
-    element.classList.remove('state-show');
-  } else {
-    element.classList.add('state-show');
-  }
-});
+const imgA = document.querySelector('#myImageA');
+imgA.src = 'images/photo_a.jpg'
 
-const targetEl = document.querySelector('.rect');
-targetEl.addEventListener('animationstart', (event) => {
-  // アニメーションが開始したときのイベント
-  document.querySelector('.log').innerHTML = 'animationstart 発生 : ' + new Date().toLocaleTimeString();
-});
-
-targetEl.addEventListener('animationiteration', (event) => {
-  // アニメーションで繰り返しが発生した時のイベント
-  // （繰り返しが未指定の場合は発生しない）
-  document.querySelector('.log').innerHTML = 'animationiteration 発生 : ' + new Date().toLocaleTimeString();
-});
-
-targetEl.addEventListener('animationend', (event) => {
-  // アニメーションが完了したときのイベント
-  // （繰り返しを指定した場合は発生しない）
-  document.querySelector('.log').innerHTML = 'animationend 発生 : ' + new Date().toLocaleTimeString();
-});
+const imgb = document.querySelector('#myImageB');
+imgb.src = 'images/photo_b.jpg'
