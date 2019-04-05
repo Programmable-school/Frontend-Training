93917e415c3607a874a7634aac34cd09bb89aad1
Shohei Nakagawa <hukusuke1007@gmail.com>
2019-04-06 07:52:57 +0900

    js

--- frontend/htmlcssjs/lesson/jquery/js/main.js
+++ frontend/htmlcssjs/lesson/jquery/js/main.js
@@ -1,3 +1,7 @@
 $(function() {
-  $("#ticker01").liScroll();
-});
+  $("#ticker01").liScroll()
+  $("#ticker01").off("mouseenter mouseleave");
+})
+
+$("#link__continue--middle").attr("href", "https://github.com/hukusuke1007")
+$("#link__continue--lower").attr("href", "https://github.com/hukusuke1007")
